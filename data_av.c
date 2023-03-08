6395852f11044d3d390966da8efe3c8d043db29c
Samuel Ngai <samuelngai@Samuels-MacBook-Pro.local>
2023-03-08 13:11:47 -0800

    improvements

--- data_av.c
+++ data_av.c
@@ -7,104 +7,54 @@
 #include <limits.h>
 
 
-void process_file(char** arg) {
+
+void process_file(char* arg) {
     //FILE* file = fopen(arg[1],"r");
-
-    char const* const fileName = arg[1];
+    //char const* const fileName = arg[1];
+    char  const* const fileName = arg;
     FILE *file = fopen(fileName, "r");
-
     if(file == NULL) {
         printf("Error\n");
         exit(0);
     }
 
-    printf("%s\n", arg[1]);
-
-    fseek(file, 0, SEEK_END);
-    int size  = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-
-    // int lines  = 0;
-    // while(!feof(file)) {
-    //     char ch = fgetc(file);
-    //     if(ch == '\n'){
-    //         lines++;
-    //     }
-    // }
-
-    size = size * sizeof(char);
-    unsigned char buf[size];
-    // printf("size  is %d\n", size);
-    // printf("buff size is %d\n", sizeof(buf));
-
-    // int data[size];
-
-    // int i =  0;
-    // printf("here\n");
-    // if(buf){
-    //     fread(buf, sizeof(buf), 1, file);
-    //     //printf("%s\n", buf);
-    //     printf("size of buf is %d\n", sizeof(buf));
-    // }
-    // char* str = strtok(buf, "\n");
-    // char* copyStr = str;
-    // printf("%s\n", copyStr);
-    // char* bingStr = strtok(copyStr, " ");
-    // printf("bingStr is %s\n", bingStr);
-    // printf("here\n");
-    // double minTemp = 1000000;
-    // double maxTemp = -1000000;
-    // char subStr[5];
-    // while(str != NULL) {
-    //     //printf("token is %s\n", str);
-    //     //printf("str length is %d\n", sizeof(str));
-    //     memcpy(subStr, str, 4);
-    //     subStr[4]  = '\0';
-    //     //printf("substr is %s\n", subStr);
-
-    //     if((double)(*subStr) > maxTemp) {
-    //         printf("substr in double is %d\n", (double)(*subStr));
-    //         maxTemp  = (double)(*subStr);
-    //     }
-
-    //     // char* copyStr = str;
-    //     // char* subStr = strtok(copyStr, "\n");
-    //     //printf("substr is %s\n", subStr);
-    //     //subStr = strtok(NULL, "\n");
-    //     str = strtok(NULL, "\n");   //should go line  by line
-    // }
-
     char line[500];
     float data[2];
-
-    float maxTemp = -10000000;
-    float minTemp = 10000000;
+    // data[0] = 0;
+    // data[1] = 0;
+    float maxTemp = -1000;
+    float minTemp = 1000;
     int entryCount = 0;
     float averageTemp = 0;
-    printf("Size is %d\n", size);
+
     if(file == NULL) {
         return;
     }
     while(fgets(line, sizeof(line), file)) {
         sscanf(line, "%f %f", &data[0], &data[1]);
+        // printf("data[0] is %f\n", data[0]);
+        // printf("maxTemp is %f\n", maxTemp);
+        // printf("data[1] is %f\n", data[1]);
+        // printf("minTemp is %f\n", minTemp);
         if(data[0] > maxTemp) {
-            maxTemp  = data[0];
+            //printf("new max fround: %f\n", data[0]);
+            //printf("%f is greater than %f\n", data[0], maxTemp);            
+            maxTemp = data[0];
         }
         if(data[1] < minTemp) {
+            //printf("new min fround: %f\n", data[1]);
             minTemp = data[1];
         }
         averageTemp += data[0] + data[1];
         entryCount++;
-        //printf("%s", line);
     }
-
+    entryCount -= 1; //offset because of the first starting label
     printf("\n");
+    printf("===file is %s===\n", fileName);
     printf("max temp is %f\n", maxTemp);
     printf("min temp is %f\n", minTemp);
     printf("average is %f\n", averageTemp/entryCount);
     printf("entry  count is %d\n", entryCount);
-    //printf("lines is %d\n", lines);
     fclose(file);
 }
 
@@ -112,7 +62,41 @@ void process_file(char** arg) {
 int
 main(int argc, char* argv[])
 {
+
+    char* filePath[10];
+    filePath[0] = "data_files/Charlottetown.dat";
+    filePath[1] = "data_files/Edmonton.dat";
+    filePath[2] = "data_files/Halifax.dat";
+    filePath[3] = "data_files/Montreal.dat";
+    filePath[4] = "data_files/Ottawa.dat";
+    filePath[5] = "data_files/Quebec.dat";
+    filePath[6] = "data_files/Toronto.dat";
+    filePath[7] = "data_files/Vancouver.dat";
+    filePath[8] = "data_files/Victoria.dat";
+    filePath[9] = "data_files/Winnipeg.dat";
     
-    process_file(argv);
+
+    if(argc == 2) {
+        char* isMultiThreading = "";
+        isMultiThreading = argv[1];
+        if(strcmp(isMultiThreading, "-m")== 0) {
+            //do multithreding
+            printf("chose multithreading\n");
+            printf("jokes multithreading doesn't do anything\n");
+            return 0;
+        }
+    }
+    //printf("filechoice is %d\n", fileChoice);
+    printf("chose: %s\n", filePath[0]);
+    process_file(filePath[0]);
+    process_file(filePath[1]);
+    process_file(filePath[2]);
+    process_file(filePath[3]);
+    process_file(filePath[4]);
+    process_file(filePath[5]);
+
+    printf("here\n");
+    //process_file(argv);
+
 	return 0;
 }
